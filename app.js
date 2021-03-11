d285f94017584dc29e1e2dcac7f048d60f4b3b54
skullkim <space1878@naver.com>
2021-03-11 14:35:03 +0900

    초기 파일 구조, 임시 엔트리포인트 완성

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+const {time} = require('console');
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    proxy: true,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: time.getMilliseconds + (100000 * 60),
+    },
+    name: "session-cookie",
+}));
+
+app.get('/', (req, res, next) => {
+    res.send('success');
+})
+
+app.use((req, res, next) => {
+    const error = new Error(`${res.method} ${req.url} router doesn't exist`);
+    error.status = 404;
+    next(error);
+})
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.messgae;
+    res.locals.error = process.env.NODE_DEV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.send(res.locals.message);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.get('port')} server start`);
+})

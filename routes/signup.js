77650139b7afd70f6840f4b49375ba6749b00b47
skullkim <space1878@naver.com>
2021-03-20 15:24:05 +0900

    회원가입 프로토타입 구현 완료

---
+++ routes/signup.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    try{
+        res.render('signup');
+    }
+    catch(err){
+        console.error(err);
+        next(err);
+    }
+});
+
+module.exports = router;

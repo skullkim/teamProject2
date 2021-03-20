a28ce3810c26c4ad6000df79baec95576f3fb9ec
skullkim <space1878@naver.com>
2021-03-20 15:17:27 +0900

    로그인페이지 프로토타입 구현 완료

--- app.js
+++ app.js
@@ -41,10 +41,12 @@ app.use(session({
 
 
 const index_router = require('./routes');
+const login_router = require('./routes/login');
 
 app.use(path.join(__dirname, '/style'), express.static('public'));
 app.use(path.join(__dirname, '/script'), express.static('public'));
 app.use('/', index_router);
+app.use('/login', login_router);
 
 app.use((req, res, next) => {
     const error = new Error(`${res.method} ${req.url} router doesn't exist`);
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     try{
-        res.render('index');
+        res.render('login');
     }
     catch(err){
         console.error(err);

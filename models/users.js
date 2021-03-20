95ba96d94d3ed00e62955d030e6046beafda945a
skullkim <space1878@naver.com>
2021-03-20 15:00:16 +0900

    users sequelize 수정

--- models/users.js
+++ models/users.js
@@ -25,6 +25,16 @@ module.exports = class User extends Sequelize.Model{
                 allowNull: false,
                 defaultValue: Sequelize.NOW,
             }
+        },{
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
         })
     }
+    static associate(db){}
 }

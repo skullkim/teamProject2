65c77d42a05889921ce9c6c13ba47305219e3eb5
skullkim <space1878@naver.com>
2021-03-22 23:49:35 +0900

    헤더 구현 완료

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     try{
-        res.render('index');
+        req.isAuthenticated() ? res.render('index', {is_logged_in: true}) : res.render('index', {is_logged_in: false});
     }
     catch(err){
         console.error(err);

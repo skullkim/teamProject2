0d0d9984fb78ac99e8a6e23fccc93e8b2daf5d6f
skullkim <space1878@naver.com>
2021-04-03 00:03:26 +0900

    로컬 로그인시 프로파일의 기본 프로파일 사진 추가, S3 기본 연동 완료

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const{isLoggedIn, isNotLoggedIn} = require('./middlewares');
+const AWS = require('aws-sdk');
+const{isLoggedIn, isNotLoggedIn, AwsConfig} = require('./middlewares');
 
 const router = express.Router();
 
@@ -15,9 +16,30 @@ router.get('/posting', isLoggedIn, (req, res, next) => {
 
 router.get('/profile', isLoggedIn, (req, res, next) => {
     try{
-        console.log(req.user);
         const {name} = req.user.dataValues;
         res.render('profile', {is_logged_in: true, user_name: name});
+    }
+    catch(err){
+        console.error(err);
+        next(err);
+    }
+});
+
+router.get('/profile-img', isLoggedIn, (req, res, next) => {
+    try{
+        const s3 = new AWS.S3();
+        s3.getObject({
+            Bucket: `${process.env.AWS_S3_BUCKET}`,
+            Key: `${process.env.DEFAULT_PROFILE_IMG_KEY}`,
+        }, (err, data) => {
+            if(err){
+                console.error(err);
+            }
+            else{
+                res.write(data.Body, 'binary');
+                res.end(null, 'binary');
+            }
+        });
     }
     catch(err){
         console.error(err);
--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+
+require('dotenv').config();
 
 exports.isLoggedIn = (req, res, next) => {
     req.isAuthenticated() ? next() : res.redirect('/?error=you have to login first');
@@ -7,3 +12,21 @@ exports.isLoggedIn = (req, res, next) => {
 exports.isNotLoggedIn = (req, res, next) => {
     !req.isAuthenticated() ? next() : res.redirect('/?error=you already logged in');
 }
+
+exports.AwsConfig = AWS.config.update({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+    region: `${process.env.AWS_region}`,
+});
+
+exports.uploadProfileImage = multer({
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: `${process.env.AWS_S3_BUCKET}`,
+        key(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, `upload/profile/local/${path.basename(file.originalname, ext) + Date.now() + ext}`);
+        },
+    }),
+});
+

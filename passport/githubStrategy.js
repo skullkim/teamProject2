ac9d6c0483140ee32e67ac2bcbb47017045ba625
skullKim <space1878@naver.com>
2021-04-09 16:06:29 +0900

    깃허브 로그인 시 깃허브 프로필 이미지 저장 기능 완료

--- passport/githubStrategy.js
+++ passport/githubStrategy.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 const githubStrategy = require('passport-github');
 const User = require('../models/users');
 const Token = require('../models/token');
+const AWS = require('aws-sdk');
+const s3Stream = require('s3-streams');
+const axios = require('axios');
 
 module.exports = () => passport.use(new githubStrategy({
     clientID: process.env.GIT_CLIENT_ID,
@@ -16,6 +19,22 @@ module.exports = () => passport.use(new githubStrategy({
         const ex_user = await User.findOne({
             where: {api_id: id, login_as}
         });
+        const img_path = profile.photos[0].value;
+        const profile_img = `github-profile-${Date.now()}.jpg`;
+        const key = `upload/profile/github/github-profile-${Date.now()}`;
+        axios.get(img_path, {
+            responseType: 'stream',
+        })
+            .then((response) => {
+                const s3 = new AWS.S3();
+                response.data.pipe(s3Stream.WriteStream(s3, {
+                    Bucket: `${process.env.AWS_S3_BUCKET}`,
+                    Key: key,
+                }));
+            })
+            .catch((err) => {
+                console.error(err);
+            });
         const date = new Date();
         let user, user_id;
         if(ex_user){
@@ -23,7 +42,8 @@ module.exports = () => passport.use(new githubStrategy({
                 {
                     name: login,
                     password: `${id}`,
-                    email: `${date.getMilliseconds()}`
+                    email: `${date.getMilliseconds()}`,
+                    profile_key: key,
                 },
                 {where: {api_id: id, login_as}}
             )
@@ -37,6 +57,7 @@ module.exports = () => passport.use(new githubStrategy({
                 email: `${date.getMilliseconds()}`,
                 login_as,
                 api_id: `${id}`,
+                profile_key: key,
             });
             user = new_user;
             user_id = new_user.id;

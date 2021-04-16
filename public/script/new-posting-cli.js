c25867697f9ae7d92a952edd53a8472c520a011f
skullKim <space1878@naver.com>
2021-04-16 13:20:55 +0900

    새로운 글 작성시 제목, 내용, 카테고리 미 선택에 대한 예외처리 완료

--- public/script/new-posting-cli.js
+++ public/script/new-posting-cli.js
@@ -1,10 +1,29 @@
 const submit_btn = document.getElementById('new-post__submit');
+const message = document.getElementById('message');
+const makeErrorMessage = (title, category, context) => {
+    if(!title){
+        message.innerText = "제목을 입력하세요";
+        return false;
+    }
+    else if(!context){
+        message.innerText = "내용을 입력하세요";
+        return false;
+    }
+    else if(category === '=== 글 카테고리 ==='){
+        message.innerText = "카테고리를 선택하세요";
+        return false;
+    }
+    return true;
+}
 submit_btn.addEventListener('click', () => {
      const title = document.getElementById('post__title').value;
      const categories = document.getElementById('post__category');
      const category = categories.options[categories.selectedIndex].value;
      const context = document.getElementById('post__main-context').value;
      console.log(title, category, context);
+     if(!makeErrorMessage(title, category, context)){
+         return;
+     }
      axios({
          method: 'put',
          url: '/auth/new-posting',

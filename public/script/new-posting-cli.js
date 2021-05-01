2d07436ba55a28e93399a694a6afac53db88d3a3
skullKim <space1878@naver.com>
2021-05-01 14:42:15 +0900

    새로운 포스팅 작성 페이지 jquery적용 완료

--- public/script/new-posting-cli.js
+++ public/script/new-posting-cli.js
@@ -1,50 +1,51 @@
-const submit_btn = document.getElementById('new-post__submit');
-const message = document.getElementById('message');
-const makeErrorMessage = (title, category, context) => {
-    if(!title){
-        message.innerText = "제목을 입력하세요";
-        return false;
+$(document).ready(() => {
+    const submit_btn = $('#new-post__submit');
+    const message = $('#message');
+    const makeErrorMessage = (title, category, context) => {
+        if(!title){
+            message.text("제목을 입력하세요")
+            return false;
+        }
+        else if(!context){
+            message.text("내용을 입력하세요");
+            return false;
+        }
+        else if(category === '=== 글 카테고리 ==='){
+            message.text("카테고리를 선택하세요");
+            return false;
+        }
+        return true;
     }
-    else if(!context){
-        message.innerText = "내용을 입력하세요";
-        return false;
-    }
-    else if(category === '=== 글 카테고리 ==='){
-        message.innerText = "카테고리를 선택하세요";
-        return false;
-    }
-    return true;
-}
-submit_btn.addEventListener('click', () => {
-     const title = document.getElementById('post__title').value;
-     const categories = document.getElementById('post__category');
-     const category = categories.options[categories.selectedIndex].value;
-     const context = document.getElementById('post__main-context').value;
-     console.log(title, category, context);
-     if(!makeErrorMessage(title, category, context)){
-         return;
-     }
-     axios({
-         method: 'put',
-         url: '/auth/new-posting',
-         contentType: 'application/json',
-         cacheControl: 'no-cache',
-         data:{
-             title,
-             category,
-             context,
-         }
-     })
-         .then((response) => {
-             const{err} = response.data;
-             if(err){
-                 message.innerText = err;
-             }
-             else{
-                 location.href="/";
-             }
-         })
-         .catch((err) => {
-             console.error(err);
-         })
+    submit_btn.click(() => {
+        const title = $('#post__title').val();
+        const categories = $('#post__category')[0];
+        const category = categories.options[categories.selectedIndex].value;
+        const context = $('#post__main-context').val();
+        if(!makeErrorMessage(title, category, context)){
+            return;
+        }
+        axios({
+            method: 'put',
+            url: '/auth/new-posting',
+            contentType: 'application/json',
+            cacheControl: 'no-cache',
+            data:{
+                title,
+                category,
+                context,
+            }
+        })
+            .then((response) => {
+                const{err} = response.data;
+                if(err){
+                    message.text(err);
+                }
+                else{
+                    location.href="/";
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    });
 });

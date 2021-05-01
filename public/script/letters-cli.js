72b8a03c9d4fe0d1b118052ac83f15ef7fab0aab
skullKim <space1878@naver.com>
2021-05-01 14:30:01 +0900

    letters페이지 jquery적용 완료

--- public/script/letters-cli.js
+++ public/script/letters-cli.js
@@ -1,21 +1,23 @@
-const main_section = document.getElementById("main__letters");
-const displayWritten = (id, category, title) => {
-    const written = document.createElement('div');
-    written.innerHTML = `
-        <h2><a href=/letter/written?written=${id}>${title}</a></h2>
-        <span>
-            category: <a href="/letter/categories?category=${category}">${category}</a>    
-        </span>
-    `;
-    main_section.appendChild(written);
-}
-axios.get('/auth/postings')
-    .then((res) => {
-        res.data.forEach((written) => {
-            const {id, main_category, title} = written;
-            displayWritten(id, main_category, title);
+$(document).ready(() => {
+    const main_section = $('#main__letters');
+    const displayWritten = (id, category, title) => {
+        const $written = $(
+        `<div>
+            <h2><a href=/letter/written?written=${id}>${title}</a></h2>
+             <span>
+                 category: <a href="/letter/categories?category=${category}">${category}</a>    
+             </span>
+        </div>`);
+        main_section.append($written);
+    }
+    axios.get('/auth/postings')
+        .then((res) => {
+            res.data.forEach((written) => {
+                const {id, main_category, title} = written;
+                displayWritten(id, main_category, title);
+            })
         })
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+        .catch((err) => {
+            console.error(err);
+        });
+})

7243acd8b012014d55bc2551a470a1ea1978ac87
skullKim <space1878@naver.com>
2021-05-01 13:51:04 +0900

    login페이지 js jquery로 변환 완료

--- public/script/login-cli.js
+++ public/script/login-cli.js
@@ -1,37 +1,40 @@
-const submit_btn = document.getElementById('login__submit');
-const checkEmail = (email) => {
-    const reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-    return !reg_email.test(email) ? false : true;
-}
-submit_btn.addEventListener('click', () =>{
-    const email = document.getElementById('local__name').value;
-    const password = document.getElementById('local__password').value;
-    const message = document.getElementById('login__err-message');
-    if(!checkEmail(email)){
-        message.innerText = "incorrect email address";
+$(document).ready(function (){
+    const submit_btn = $('#login__submit');
+    const checkEmail = (email) => {
+        const reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+        return !reg_email.test(email) ? false : true;
     }
-    else{
-        axios({
-            method: 'post',
-            url: '/login/confirm-login',
-            contentType: 'application/json',
-            cacheControl: 'no-cache',
-            data:{
-                email,
-                password,
-            }
-        })
-            .then((response) => {
-                const {err} = response.data;
-                if(err){
-                    message.innerText = err;
-                }
-                else{
-                    location.href='/';
+    submit_btn.click( () => {
+        const email = $('#local__name').val();
+        const password = $('#local__password').val();
+        const message = $('#login__err-message');
+        console.log(email, password);
+        if(!checkEmail(email)){
+            message.text("incorrect email address");
+        }
+        else {
+            axios({
+                method: 'post',
+                url: '/login/confirm-login',
+                contentType: 'application/json',
+                cacheControl: 'no-cache',
+                data: {
+                    email,
+                    password,
                 }
             })
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-});
+                .then((response) => {
+                    const {err} = response.data;
+                    if (err) {
+                        //message.innerText = err;
+                        message.text(err);
+                    } else {
+                        location.href = '/';
+                    }
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+    });
+})

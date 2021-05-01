ef11189c27c27e62e5759449dc099b126dcef652
skullKim <space1878@naver.com>
2021-05-01 14:23:44 +0900

    프로필 변경 페이지 jquery로 변환 완료

--- public/script/edit-profile-cli.js
+++ public/script/edit-profile-cli.js
@@ -1,20 +1,22 @@
-axios({
-    method: 'get',
-    url: '/auth/user-info',
-    contentType: 'application/json',
-    cacheControl: 'no-cache',
-})
-    .then((response) => {
-        //console.log(response);
-        const {name, email, age} = response.data;
-        const name_node = document.getElementById('edit-profile__name');
-        const email_node = document.getElementById('edit-profile__email');
-        const age_node = document.getElementById('edit-profile__age');
-        name_node.setAttribute('placeholder', `name: ${name}`);
-        email_node.setAttribute('placeholder', `email: ${email}`);
-        age_node.setAttribute('placeholder', `age: ${age}`);
+$(document).ready(() => {
+    axios({
+        method: 'get',
+        url: '/auth/user-info',
+        contentType: 'application/json',
+        cacheControl: 'no-cache',
     })
-    .catch((err) => {
-        console.error(err);
-    });
+        .then((response) => {
+            const {name, email, age} = response.data;
+            const name_node = $('#edit-profile__name');
+            const email_node = $('#edit-profile__email');
+            const age_node = $('#edit-profile__age');
+            name_node.attr('placeholder', `name: ${name}`);
+            email_node.attr('placeholder', `email: ${email}`);
+            age_node.attr('placeholder', `age: ${age}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+})
 

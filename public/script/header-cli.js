11cf7908706452cabb7410001643f9c234064437
skullKim <space1878@naver.com>
2021-05-10 19:57:35 +0900

    글검색어, 검색 카테고리 추출 완료

--- public/script/header-cli.js
+++ public/script/header-cli.js
@@ -1,2 +1,17 @@
 $(document).ready(() => {
+    const getSearchScope = () => {
+        const search_scope = $('#search-list')[0];
+        return search_scope.options[search_scope.selectedIndex].value;
+    }
+    const search_btn = $('#head__search-logo');
+    search_btn.click(() => {
+        const search_target = $('#head__search').val();
+        const search_scope = getSearchScope();
+       if(!search_scope){
+           alert('검색 범위를 선택하세요');
+       }
+       else if(!search_target){
+           alert('검색어를 입력하세요');
+       }
+    })
 })

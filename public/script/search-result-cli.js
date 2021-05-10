0491d4b842ddac31470f482ef4cef6525b8428c3
skullKim <space1878@naver.com>
2021-05-10 20:55:55 +0900

    검색결과 페이지에서 전체 카테고리 조회부분 완료

--- public/script/search-result-cli.js
+++ public/script/search-result-cli.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
-   console.log(1);
+    axios.get('/letter/categories')
+        .then((res) => {
+            const categories = res.data;
+            const aside = $('#main__category');
+            $.each(categories, (key, value) => {
+                aside.append(`<p><a href="">${key}</a></p>`);
+                const ul = $('<ul></ul>');
+                value.forEach((tag) => {
+                    ul.append(`<li><a href="">${tag}</a></li>`);
+                });
+                aside.append(ul);
+                //console.log(key, value);
+            })
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 });

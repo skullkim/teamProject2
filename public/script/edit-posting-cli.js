b9126bb24efdcd273ff4f46e03b2aa760fc366fe
skullKim <space1878@naver.com>
2021-05-19 14:29:41 +0900

    로그인 후 글  수정페이지에서 기존 제목, 글내용 조회기능 완료

--- public/script/edit-posting-cli.js
+++ public/script/edit-posting-cli.js
@@ -1,5 +1,36 @@
 $(document).ready(() => {
    const url = (new URL(location.href)).searchParams;
    const written = url.get('written');
-   console.log(written);
+   // let context;
+   // let all_tags;
+    const post = $('#main_post');
+   function displayPrevContext(context, all_tags){
+        const{author, title, main_posting, main_category} = context.main_data;
+       $('#post__title').val(`${title}`)
+        $('#post__main-context').val(`${main_posting}`);
+   }
+   axios({
+       method: 'get',
+       url: `/letter/letter-context?written=${written}`,
+       contentType: 'application/json',
+       cacheControl: 'no-cache',
+   })
+       .then((context_res) => {
+           axios({
+               method: 'get',
+               url: '/letter/categories',
+               contentType: 'application/json',
+               cacheControl: 'nocache',
+           })
+               .then((tags_response) => {
+                   displayPrevContext(context_res.data, tags_response.data);
+
+               })
+               .catch((err) => {
+                   console.error(err);
+               });
+       })
+       .catch((err) => {
+           console.error(err);
+       });
 });

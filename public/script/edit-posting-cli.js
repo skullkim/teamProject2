e43739057e25d162be918e7108b35c80746a6172
skullKim <space1878@naver.com>
2021-05-19 15:05:43 +0900

    개인글 수정 페이지에 들어가면 기존에 작성한 내용 불러오기완료

--- public/script/edit-posting-cli.js
+++ public/script/edit-posting-cli.js
@@ -1,14 +1,32 @@
 $(document).ready(() => {
    const url = (new URL(location.href)).searchParams;
    const written = url.get('written');
-   // let context;
-   // let all_tags;
     const post = $('#main_post');
+    //기존에 작성된 정보 로딩
    function displayPrevContext(context, all_tags){
         const{author, title, main_posting, main_category} = context.main_data;
-       $('#post__title').val(`${title}`)
-        $('#post__main-context').val(`${main_posting}`);
+       $('#post__title').val(`${title}`)//제목
+        $('#post__main-context').val(`${main_posting}`);//글 내용
+       $('#post__category').val(`${main_category}`).prop('selected', true);//메인 카테고리
+       const tags = $('#post__tag');
+       let ids = 0;
+       //태그
+       all_tags[`${main_category}`].forEach((tag) => {
+           const id = `tag__${ids++}`;
+           const new_tag = $(`
+                <input type="checkbox" id="${id}" value="${tag}" name="tag-box"/>
+                <label for="${id}">${tag}</label>
+            `);
+           context.tags.forEach((selected) => {
+               if(tag == selected){
+                   new_tag.prop('checked', true);
+               }
+           });
+           tags.append(new_tag);
+       });
+       console.log(all_tags[`${main_category}`], context.tags);
    }
+   //글 내용과 태그를 가져온다
    axios({
        method: 'get',
        url: `/letter/letter-context?written=${written}`,

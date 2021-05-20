35e069a145dac5573e9113493992544650b7965f
skullKim <space1878@naver.com>
2021-05-20 20:27:19 +0900

    카테고리 클릭 시 해당 카테고리 검색되는 기능 완료

--- public/script/search-result-cli.js
+++ public/script/search-result-cli.js
@@ -15,10 +15,10 @@ $(document).ready(() => {
             const categories = res.data;
             const aside = $('#main__category');
             $.each(categories, (key, value) => {
-                aside.append(`<p><a href="">${key}</a></p>`);
+                aside.append(`<p><a href="/letter/result?scope=category&target=${key}">${key}</a></p>`);
                 const ul = $('<ul></ul>');
                 value.forEach((tag) => {
-                    ul.append(`<li><a href="">${tag}</a></li>`);
+                    ul.append(`<li><a href="/letter/result?scope=category&target=${tag}">${tag}</a></li>`);
                 });
                 aside.append(ul);
             });

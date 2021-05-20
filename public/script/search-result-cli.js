5a9a17900495c5cb132936fe2479276175369e1e
skullKim <space1878@naver.com>
2021-05-20 21:41:51 +0900

    도서 API를 통한 책 추천 클라이언트에서 책 정보 조회 기능 완료

--- public/script/search-result-cli.js
+++ public/script/search-result-cli.js
@@ -12,11 +12,10 @@ $(document).ready(() => {
     }
     const displayBook = (books) => {
         books.forEach((book) => {
-            const{authors, thumbnail, title, contents} = book;
-            console.log(authors, thumbnail, title, contents);
+            const{authors, thumbnail, title, contents, url} = book;
             const book_intro = $(`
                     <div>
-                        <p>제목:${title}</p>
+                        <p><a href="${url}">제목:${title}</a></p>
                         <img src="${thumbnail}" alt="book thumbnail">
                         <p>저자:${authors}</p>
                         <p>${contents}</p>

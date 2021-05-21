d9107222746c02cea877fdf580ce78f695c1e350
skullKim <space1878@naver.com>
2021-05-22 01:08:01 +0900

    프로필에서 자신이 작성한 댓글 조회기능, 수정 삭제버튼 표시 완료

--- public/script/profile-cli.js
+++ public/script/profile-cli.js
@@ -12,6 +12,17 @@ $(document).ready(() => {
         </div>`);
         my_postings.append($written);
     }
+    const my_comments = $('#user-info__comments');
+    const displayComment = (id, comment) => {
+        const $div = $(`
+            <div>
+                <span>댓글: ${comment}</span>
+                <button id="comments__edit"><a href="/auth/edit-comment?id=${id}">수정</a></button>
+                <button id="comments__remove"><a href="/auth/remove-comment?id=${id}">삭제</a></button>
+            </div>
+        `);
+        my_comments.append($div);
+    }
     axios({
         method: 'get',
         url: '/auth/wrote-postings',
@@ -35,6 +46,11 @@ $(document).ready(() => {
     })
         .then((response) => {
             console.log(response);
+            response.data.forEach((data) => {
+                const {id, comment} = data;
+                console.log(id, comment);
+                displayComment(id, comment);
+            })
         })
         .catch((err) => {
             console.error(err);
